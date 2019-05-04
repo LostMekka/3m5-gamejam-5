c2b527c11b27bc20241128b2b2d8f5d11c610544
Gilbert Roehrbein <gilbert.rohrbein@logmein.com>
2019-05-04 15:23:27 +0200

    add basic Dude class

--- core/src/de/lostmekka/_3m5/gamejam/_5/GamePlayScreen.kt
+++ core/src/de/lostmekka/_3m5/gamejam/_5/GamePlayScreen.kt
@@ -9,18 +9,21 @@ import ktx.graphics.use
 
 class GamePlayScreen : KtxScreen {
     val batch = SpriteBatch()
-    private val img = Texture("badlogic.jpg")
 
     override fun render(delta: Float) {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        var dude = Dude()
+        dude.setPosition(100f, 100f)
+
+
         batch.use {
-            batch.draw(img, 0f, 0f)
+            dude.draw(batch, 1f)
         }
     }
 
     override fun dispose() {
         batch.dispose()
-        img.dispose()
     }
 }
