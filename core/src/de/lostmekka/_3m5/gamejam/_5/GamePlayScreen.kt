93aa8f0b7c34448c0d6a1cd575bda6807e1ab7c6
LostMekka <lostmekka@users.noreply.github.com>
2019-05-04 14:00:59 +0200

    main structure for gameplay screen + mogul entity

--- core/src/de/lostmekka/_3m5/gamejam/_5/GamePlayScreen.kt
+++ core/src/de/lostmekka/_3m5/gamejam/_5/GamePlayScreen.kt
@@ -4,19 +4,57 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.utils.viewport.ExtendViewport
+import de.lostmekka._3m5.gamejam._5.entity.Mogul
 import ktx.app.KtxScreen
+import ktx.graphics.circle
 import ktx.graphics.use
+
+const val MogulRadius = 0.4f
 
 class GamePlayScreen : KtxScreen {
     private val batch = SpriteBatch()
+    private val shapeRenderer = ShapeRenderer()
+
     private val img = Texture("badlogic.jpg")
 
-    override fun render(delta: Float) {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+    private val viewport = ExtendViewport(20f, 10f)
+    private val mogul = Mogul()
+    private val stage = Stage(viewport).apply {
+        addActor(mogul)
+    }
+
+    private fun handleInput(delta: Float) {
+        // TODO
+    }
+
+    private fun update(delta: Float) {
+        stage.camera.position.set(0f, 0f, stage.camera.position.z)
+        stage.act(delta)
+    }
+
+    private fun draw() {
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.use {
-            batch.draw(img, 0f, 0f)
+        stage.draw()
+
+        shapeRenderer.use(ShapeRenderer.ShapeType.Line) {
+            it.projectionMatrix = stage.camera.projection
+            it.setAutoShapeType(true)
+            it.circle(mogul.x, mogul.y, MogulRadius, 20)
         }
+    }
+
+    override fun render(delta: Float) {
+        handleInput(delta)
+        update(delta)
+        draw()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        stage.viewport.update(width, height, true)
     }
 
     override fun dispose() {
---
+++ core/src/de/lostmekka/_3m5/gamejam/_5/entity/Mogul.kt
@@ -0,0 +1,10 @@
+package de.lostmekka._3m5.gamejam._5.entity
+
+import com.badlogic.gdx.scenes.scene2d.Actor
+
+/**
+ * Copyright 2019 LostMekkaSoft
+ */
+class Mogul: Actor() {
+
+}
