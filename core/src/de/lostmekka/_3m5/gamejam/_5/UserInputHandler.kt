dc7e78a7259fd20408d3a8050a9d9dff098f2495
KnrdRaue <raue.konrad@t-online.de>
2019-05-05 19:02:10 +0200

    Building structures now costs ressources

--- core/src/de/lostmekka/_3m5/gamejam/_5/UserInputHandler.kt
+++ core/src/de/lostmekka/_3m5/gamejam/_5/UserInputHandler.kt
@@ -70,7 +70,7 @@ class UserInputHandler(
         val distance = buildCaravanPostDistance(state.source)
         var sourceIsNear = state.source.position.dst(coords) < distance
 
-        return mogulIsNear && sourceIsNear
+        return mogulIsNear && sourceIsNear && (slaves >=10)
     }
 
     private fun canBuildTower(state: State.BuildTower): Boolean {
@@ -78,7 +78,7 @@ class UserInputHandler(
         val caravanPostIsNear = isNear<CaravanPost>(coords, buildCaravanPostPostDistance)
         val magistrateIsNear = isNear<Magistrate>(coords, buildCaravanPostMagistrateDistance)
 
-        return mogulIsNear && (caravanPostIsNear || magistrateIsNear)
+        return mogulIsNear && (caravanPostIsNear || magistrateIsNear)&&(slaves >= 25)
     }
 
     private fun handleSecondaryAction(coords: Vector2) {
@@ -136,11 +136,13 @@ class UserInputHandler(
                 Sounds.build.play()
             }
         }
+        slaves -= 10
     }
 
     private fun buildTower(coords: Vector2) {
         stage.addActor(createTower(coords))
         Sounds.build.play()
+        slaves -=25
     }
 
     private inline fun <reified T> isNear(coords: Vector2, dst: Float): Boolean {
