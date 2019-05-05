a286b19bfbd39697b1c541620a7f7c337b15ee7e
KnrdRaue <raue.konrad@t-online.de>
2019-05-05 11:24:10 +0200

    Added first attempt at the movement of dudes

--- core/src/de/lostmekka/_3m5/gamejam/_5/entity/Dude.kt
+++ core/src/de/lostmekka/_3m5/gamejam/_5/entity/Dude.kt
@@ -4,11 +4,15 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.World
+import de.lostmekka._3m5.gamejam._5.dudeSpeed
 import ktx.box2d.Query
 import ktx.box2d.body
 import ktx.box2d.query
 import de.lostmekka._3m5.gamejam._5.dudesight
 import de.lostmekka._3m5.gamejam._5.dudeHP
+import ktx.math.minus
+import ktx.math.times
+import ktx.math.vec2
 
 class Dude(world: World) : PhysicsBodyActor(world) {
     private val img = Texture("badlogic.jpg")
@@ -37,7 +41,25 @@ class Dude(world: World) : PhysicsBodyActor(world) {
         val tower = towers.minBy {
             Vector2(it.x, it.y).dst2(body.position)
         }
-    }
+
+        if(tower != null){
+            val pos = vec2(x, y)
+            val target = vec2(tower.position.x, tower.position.y)
+            val distanceToTarget = pos.dst(target)
+            val distanceThisFrame = dt * dudeSpeed
+            if (distanceThisFrame >= distanceToTarget) {
+                setPosition(target.x, target.y)
+
+            } else {
+                val movement = (target - pos) * (distanceThisFrame / distanceToTarget)
+                x += movement.x
+                y += movement.y
+                }
+            }
+        }
+
+
+
 
     fun attacked() {
         hp--
